b658ae1fae3653977c1a094a9a359e2eb8f126ba
davidm-oluwole999 <davidmoluwole@gmail.com>
2024-09-01 18:03:11 +0100

    Add files via upload

---
+++ HomeworkOne.py
@@ -0,0 +1,8 @@
+string1=input("Enter your age:  ")
+string2=input("Enter your name:  ")
+string3=input("Enter your test marks:  ")
+string4=input("Enter your favourite food:  ")
+
+you={"Age":string1, "Name":string2, "Results":string3, "Favourite-food":string4}
+
+print(you)
